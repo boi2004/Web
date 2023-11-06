5b540395acd67c2d86b962b7bb61cfdaf73deb44
boi2004 <alorota24@gmail.com>
2023-11-06 23:56:08 +0700

    Do An

---
+++ Demo/Controllers/DanhsachkhachhangController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private DPSportStoreEntities database = new DPSportStoreEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            // Lấy danh sách khách hàng từ cơ sở dữ liệu và trả về view
+            var customers = database.Customers.ToList();
+            return View(customers);
+        }
+
+        // GET: Customers/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // Hiển thị form để tạo mới khách hàng
+            return View();
+        }
+
+        // POST: Customers/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Thêm khách hàng mới vào cơ sở dữ liệu và chuyển hướng đến trang danh sách
+                    database.Customers.Add(customer);
+                    database.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    // Nếu có lỗi, hiển thị thông báo lỗi
+                    ModelState.AddModelError("", "Lỗi tạo mới khách hàng: " + ex.Message);
+                }
+            }
+
+            // Trả về view tạo mới khách hàng với thông tin và thông báo lỗi (nếu có)
+            return View(customer);
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int id)
+        {
+            var customer = database.Customers.FirstOrDefault(c => c.IDCus == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            // Hiển thị thông tin chi tiết về khách hàng
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var customer = database.Customers.FirstOrDefault(c => c.IDCus == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            // Hiển thị form chỉnh sửa khách hàng
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Cập nhật thông tin khách hàng và chuyển hướng đến trang danh sách
+                    database.Entry(customer).State = EntityState.Modified;
+                    database.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    // Nếu có lỗi, hiển thị thông báo lỗi
+                    ModelState.AddModelError("", "Lỗi chỉnh sửa khách hàng: " + ex.Message);
+                }
+            }
+
+            // Trả về view chỉnh sửa khách hàng với thông tin và thông báo lỗi (nếu có)
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var customer = database.Customers.FirstOrDefault(c => c.IDCus == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            // Hiển thị thông tin khách hàng để xác nhận xóa
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                // Xóa khách hàng khỏi cơ sở dữ liệu và chuyển hướng đến trang danh sách
+                var customer = database.Customers.FirstOrDefault(c => c.IDCus == id);
+                database.Customers.Remove(customer);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                // Nếu có lỗi, hiển thị thông báo lỗi
+                ModelState.AddModelError("", "Không xóa được do có liên quan đến bảng khác: " + ex.Message);
+                var deletedCustomer = new Customer { IDCus = id };
+                return View(deletedCustomer);
+            }
+        }
+    }
+}
---
+++ Demo/Controllers/RingController.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class RingController : Controller
+    {
+        public DPSportStoreEntities db = new DPSportStoreEntities();
+        //View để thêm sản phẩm 
+        public ActionResult AddRing ()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Controler để thêm sản phẩm
+        public ActionResult AddRing(Ring ring, HttpPostedFileBase imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageFile != null)
+                {
+                    // Lưu hình ảnh mới nếu được tải lên
+                    string fileName = Path.GetFileName(imageFile.FileName);
+                    string filePath = "~/Content/Img/" + fileName;
+                    string serverPath = Server.MapPath(filePath);
+                    imageFile.SaveAs(serverPath);
+                    ring.ImagePro = filePath;
+                }
+
+                // Thêm sản phẩm mới vào cơ sở dữ liệu
+                db.Rings.Add(ring);
+                db.SaveChanges();
+                return RedirectToAction("SeeRing"); // Chuyển hướng đến trang danh sách sản phẩm
+            }
+
+            return View(ring); // Trở lại trang chỉnh sửa nếu ModelState không hợp lệ
+        }
+        //View để xem sản phẩm
+        public ActionResult SeeRing()
+        {
+            var rings = db.Rings.ToList();
+            return View(rings);
+        }
+        //View để Edit sản phẩm
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Ring ring = db.Rings.Find(id);
+
+            if (ring == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(ring);
+        }
+        // POST: Sửa sản phẩm Ring
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Ring ring, HttpPostedFileBase imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageFile != null)
+                {
+                    // Lưu hình ảnh mới nếu được tải lên
+                    string fileName = Path.GetFileName(imageFile.FileName);
+                    string filePath = "~/Content/Img/" + fileName;
+                    string serverPath = Server.MapPath(filePath);
+                    imageFile.SaveAs(serverPath);
+                    ring.ImagePro = filePath;
+                }
+
+                // Cập nhật thông tin sản phẩm và lưu vào cơ sở dữ liệu
+                db.Entry(ring).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("SeeRing");
+            }
+
+            return View(ring);
+        }
+       //Xóa sản phẩm
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ring ring = db.Rings.Find(id);
+            if (ring == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ring);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Ring ring = db.Rings.Find(id);
+            db.Rings.Remove(ring);
+            db.SaveChanges();
+            return RedirectToAction("SeeRing");
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Ring ring = db.Rings.Find(id);
+
+            if (ring == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(ring);
+        }
+        //Danh sách sản phẩm người dùng
+        public ActionResult Gocnhinnguoidung()
+        {
+            var rings = db.Rings.ToList();
+            return View(rings);
+        }
+
+        // Giải phóng tài nguyên khi Controller hoàn thành
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        //Lọc theo giá tiền:
+        public ActionResult FilterByPrice(decimal? minPrice, decimal? maxPrice)
+        {
+            // Khởi tạo danh sách sản phẩm
+            var products = db.Rings.ToList();
+
+            // Lọc sản phẩm theo giá tiền
+            if (minPrice.HasValue)
+            {
+                products = products.Where(p => p.Price >= minPrice.Value).ToList();
+            }
+
+            if (maxPrice.HasValue)
+            {
+                products = products.Where(p => p.Price <= maxPrice.Value).ToList();
+            }
+
+            return View("Gocnhinnguoidung", products);
+        }
+        //Lọc theo giới tính:
+        public ActionResult FilterByGender(string gender)
+        {
+            // Khởi tạo danh sách sản phẩm
+            var products = db.Rings.ToList();
+
+            // Lọc sản phẩm theo giới tính
+            if (!string.IsNullOrEmpty(gender))
+            {
+                products = products.Where(p => p.Gender == gender).ToList();
+            }
+
+            return View("Gocnhinnguoidung", products);
+        }
+
+        //Lọc theo loại:
+        public ActionResult FilterByCategory(string category)
+        {
+            // Khởi tạo danh sách sản phẩm
+            var products = db.Rings.ToList();
+
+            // Lọc sản phẩm theo loại
+            if (!string.IsNullOrEmpty(category))
+            {
+                products = products.Where(p => p.JewelryType == category).ToList();
+            }
+
+            return View("Gocnhinnguoidung", products);
+        }
+
+
+    }
+
+}
